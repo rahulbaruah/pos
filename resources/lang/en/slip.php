0f2bd759c90dc97d4eda7eb03158efdd1e1a78d2
System Administrator <root@n00b.local>
2018-08-15 00:54:10 +0530

    First initialization

---
+++ resources/lang/en/slip.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	"date" => "Date",
+	"time" => "Time",
+	"invoice_no" => "Invoice No",
+	"s_no" => "S.No.",
+	"menu" => "Menu",
+	"unit_price" => "Unit Price",
+	"qty" => "Qty",
+	"total" => "Total",
+	"tax" => "GST",
+	"total_paid" => "Total Paid",
+	"payment_with" => "Payment With",
+	"grand_total" => "GRAND TOTAL",
+	"thanks_visting" => "Thanks Visit Again",
+	"back" => "Back",
+	"discount" => "Discount",
+	"total_given" => "Total Paid",
+];

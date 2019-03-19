b530a363c1f1497b558eb092602d7592df0e4632
rahulbaruah <baruah.rahul.88@gmail.com>
2019-03-19 10:47:03 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-			SalesSeeder::class,
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@xcraft.co',
+            'password' => bcrypt('password'),
+        ]);
+		
+		DB::table('settings')->insert([
+			['key' => 'title', 'label' => 'Site Title'],
+			['key' => 'phone', 'label' => 'Phone'],
+			['key' => 'email', 'label' => 'Email'],
+			['key' => 'address', 'label' => 'Address'],
+			['key' => 'country', 'label' => 'Country'],
+			['key' => 'timing1', 'label' => 'Monday To Saturday', 'value' => '11-30 am to 10-30pm'],
+			['key' => 'sunday', 'label' => 'Sunday', 'value' => 'open'],
+			['key' => 'facebook', 'label' => 'Facebook'],
+			['key' => 'twitter', 'label' => 'Twitter'],
+			['key' => 'vat', 'label' => 'GST(%)', 'value' => 5],
+			['key' => 'scharge', 'label' => 'Service Charge (%)', 'value' => 0],
+			['key' => 'delivery_cost', 'label' => 'Delivery Cost', 'value' => 0],
+			['key' => 'currency', 'label' => 'Currency', 'value' => 'Rs.'],
+			['key' => 'lng', 'label' => 'Longitude'],
+			['key' => 'lat', 'label' => 'Latitude'],
+			['key' => 'stripe', 'label' => 'Stripe Payment', 'value' => 'no'],
+			['key' => 'frontend', 'label' => 'Hide Frontend', 'value' => 'yes'],
+			['key' => 'promotions', 'label' => 'Receipt Message', 'value' => 'this is a receipt message edit'],
+			['key' => 'discount', 'label' => 'Discount', 'value' => 0],
+			['key' => 'printer1', 'label' => 'Printer 1'],
+			['key' => 'printer2', 'label' => 'Printer 2'],
+			['key' => 'gstin', 'label' => 'GSTIN'],
+			['key' => 'r_email_1', 'label' => 'Report Email 1'],
+			['key' => 'r_email_2', 'label' => 'Report Email 2']
 		]);
     }
 }

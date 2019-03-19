69fccfe4e28aa8dcbace64e01efaaf7031f1f66a
rahulbaruah <baruah.rahul.88@gmail.com>
2019-03-19 10:50:10 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,30 +18,30 @@ class DatabaseSeeder extends Seeder
         ]);
 		
 		DB::table('settings')->insert([
-			['key' => 'title', 'label' => 'Site Title'],
-			['key' => 'phone', 'label' => 'Phone'],
-			['key' => 'email', 'label' => 'Email'],
-			['key' => 'address', 'label' => 'Address'],
-			['key' => 'country', 'label' => 'Country'],
+			['key' => 'title', 'label' => 'Site Title', 'value' => ''],
+			['key' => 'phone', 'label' => 'Phone', 'value' => ''],
+			['key' => 'email', 'label' => 'Email', 'value' => ''],
+			['key' => 'address', 'label' => 'Address', 'value' => ''],
+			['key' => 'country', 'label' => 'Country', 'value' => ''],
 			['key' => 'timing1', 'label' => 'Monday To Saturday', 'value' => '11-30 am to 10-30pm'],
 			['key' => 'sunday', 'label' => 'Sunday', 'value' => 'open'],
-			['key' => 'facebook', 'label' => 'Facebook'],
-			['key' => 'twitter', 'label' => 'Twitter'],
+			['key' => 'facebook', 'label' => 'Facebook', 'value' => ''],
+			['key' => 'twitter', 'label' => 'Twitter', 'value' => ''],
 			['key' => 'vat', 'label' => 'GST(%)', 'value' => 5],
 			['key' => 'scharge', 'label' => 'Service Charge (%)', 'value' => 0],
 			['key' => 'delivery_cost', 'label' => 'Delivery Cost', 'value' => 0],
 			['key' => 'currency', 'label' => 'Currency', 'value' => 'Rs.'],
-			['key' => 'lng', 'label' => 'Longitude'],
-			['key' => 'lat', 'label' => 'Latitude'],
+			['key' => 'lng', 'label' => 'Longitude', 'value' => ''],
+			['key' => 'lat', 'label' => 'Latitude', 'value' => ''],
 			['key' => 'stripe', 'label' => 'Stripe Payment', 'value' => 'no'],
 			['key' => 'frontend', 'label' => 'Hide Frontend', 'value' => 'yes'],
 			['key' => 'promotions', 'label' => 'Receipt Message', 'value' => 'this is a receipt message edit'],
 			['key' => 'discount', 'label' => 'Discount', 'value' => 0],
-			['key' => 'printer1', 'label' => 'Printer 1'],
-			['key' => 'printer2', 'label' => 'Printer 2'],
-			['key' => 'gstin', 'label' => 'GSTIN'],
-			['key' => 'r_email_1', 'label' => 'Report Email 1'],
-			['key' => 'r_email_2', 'label' => 'Report Email 2']
+			['key' => 'printer1', 'label' => 'Printer 1', 'value' => ''],
+			['key' => 'printer2', 'label' => 'Printer 2', 'value' => ''],
+			['key' => 'gstin', 'label' => 'GSTIN', 'value' => ''],
+			['key' => 'r_email_1', 'label' => 'Report Email 1', 'value' => ''],
+			['key' => 'r_email_2', 'label' => 'Report Email 2', 'value' => '']
 		]);
     }
 }
